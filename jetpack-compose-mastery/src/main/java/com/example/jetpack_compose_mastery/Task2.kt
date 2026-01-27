d002a96bd667429bc4187fddcef79c7bb5adef59
Aayan Patel <aayan.patel@ext.dazn.com>
2026-01-27 17:07:48 +0530

    JetPackComposeMastery (Task2) Done

---
+++ jetpack-compose-mastery/src/main/java/com/example/jetpack_compose_mastery/Task2.kt
@@ -0,0 +1,12 @@
+package com.example.jetpack_compose_mastery
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.jetpack_compose_mastery.task2_screens.ProductCard
+
+@Composable
+fun Task2() {
+        ProductCard()
+}
---
+++ jetpack-compose-mastery/src/main/java/com/example/jetpack_compose_mastery/task2_screens/ProductCard.kt
@@ -0,0 +1,50 @@
+package com.example.jetpack_compose_mastery.task2_screens
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ProductCard() {
+    Box(
+        modifier = Modifier
+            .padding(16.dp)
+            .shadow(8.dp, RoundedCornerShape(12.dp))
+            .border(1.dp, Color.Gray, RoundedCornerShape(12.dp))
+            .padding(16.dp)
+    ) {
+
+        Column {
+            Text(
+                text = "Marvel Movies",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text("Captain America")
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = "₹250",
+                fontWeight = FontWeight.Bold,
+                color = Color.Blue
+            )
+        }
+    }
+}
